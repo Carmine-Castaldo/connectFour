3eae6f339480208147b2b833c09bdb799a4bd934
Carmine-Castaldo <carmine.castaldo2@studenti.unina.it>
2026-07-29 09:14:28 +0200

    inizio sviluppo client, manca la gestione dei messaggi e i controlli se le socket/thread falliscono

--- client/controller/network_controller.py
+++ client/controller/network_controller.py
@@ -4,38 +4,53 @@ import threading
 
 class NetworkClient:
 
-    def init(self, host='127.0.0.1', port=8080):
+    def __init__(self, host='127.0.0.1', port=8080):
         self.host = host
         self.port = port
         self.socket = None
-        self.running = False
         
-
     def connect(self):
-        #TO DO
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+
+        thread_connection = threading.Thread(target=self.listen_server, daemon=True)
+        thread_connection.start()
+
+
+    def listen_server(self):
+        while True:
+            self.recv_msg()
+            if not self.msg_recv:
+                print("Connessione interrota")
+                self.running = False
+                #self.disconnect() da vedere
+                break
+
+            self.handle_msg()
+
+                
+    def handle_msg(self):
+        #
         pass
 
     def recv_msg(self):
-        #TO DO
-        pass
+        self.msg_recv = self.socket.recv(1024).decode('utf-8')
+        
 
-    def send_msg(self):
-        #TO DO
-        pass
+    def send_msg(self, msg):
+        self.socket.sendall(msg.encode('utf-8'))
+        
     def create_match(self):
-        #TO DO
-        pass
+        self.send_msg('CREATE')
 
-    def join_match(self):
-        #TO DO
-        pass
+    def join_match(self, id_match: int):
+        self.send_msg(f'JOIN{id_match}')
        
     def move(self, col: int):
-        #TO DO
-        pass
-
+        self.send_msg(f'MOVE {col}')
+        
     def disconnect(self):
-        #TO DO 
-        pass
+        self.send_msg('DISCONNECT')
+        
 
 
